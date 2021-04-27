08ad2d826a24318dcd33e989503b090108046edf
matthieu LABBAYE <matthieu.labbaye@free.fr>
2021-04-27 17:19:44 +0200

    video 101 edit en cours

--- src/app/components/shop/edit-product/edit-product.component.ts
+++ src/app/components/shop/edit-product/edit-product.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-edit-product',
@@ -7,7 +11,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProductComponent implements OnInit {
 
-  constructor() { }
+  productForm: FormGroup;
+  errorMessage: string;
+  loading: boolean = false;
+  imagePreview: string;
+  constructor(private formBuilder: FormBuilder,
+    private auth: AuthService,
+    private productService: ProductService,
+    private router: Router,) { }
 
   ngOnInit(): void {
   }
