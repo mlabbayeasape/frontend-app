fb12b44c17f9030830e4c377dbab314a8aa6498d
mlabbayeasape <m.labbaye@asape.fr>
2021-04-28 11:08:50 +0200

    video 106 OK

--- src/app/components/partials/modal/delete-product-modal/delete-product-modal.component.ts
+++ src/app/components/partials/modal/delete-product-modal/delete-product-modal.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/models/product';
+import { AuthService } from 'src/app/services/auth.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-delete-product-modal',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteProductModalComponent implements OnInit {
 
-  constructor() { }
+  @Input() product: Product;
+  userId: string;
+
+  constructor(private auth: AuthService,
+              private productService: ProductService,
+              private router: Router,
+    ) { }
 
   ngOnInit(): void {
+    this.userId = this.auth.userID;
+  }
+
+  deleteProduct(product: Product){
+    if(this.userId !== product.userId){
+      return this.router.navigate(['/not-found'])
+    };
+    this.productService.deleteProduct(product._id)
+    .then(
+      () => {
+        console.log('porduct deleted')
+      }
+    )
+    .catch(
+      ()=>{
+        return this.router.navigate(['/shop'])
+      }
+    );
+
+
   }
 
 }
